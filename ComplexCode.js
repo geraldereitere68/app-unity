1b8d61167089b70bb2b6e1f29de1c635ffb1c158
geraldereitere68 <geraldereitere68@gmail.com>
2023-12-02 09:53:25 +0000

    Initial commit

---
+++ ComplexCode.js
@@ -0,0 +1,85 @@
+/*
+Filename: ComplexCode.js
+
+Description: This code demonstrates a complex JavaScript program that utilizes object-oriented programming, data structures, and various algorithms to solve a real-world problem.
+
+*/
+
+// Define a class to represent a car
+class Car {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  // Method to start the car
+  start() {
+    console.log(`Starting ${this.brand} ${this.model}...`);
+  }
+
+  // Method to stop the car
+  stop() {
+    console.log(`Stopping ${this.brand} ${this.model}...`);
+  }
+}
+
+// Create instances of Car
+const car1 = new Car("Toyota", "Camry", 2022);
+const car2 = new Car("Honda", "Accord", 2021);
+
+// Define a class to represent a car dealership
+class Dealership {
+  constructor(name) {
+    this.name = name;
+    this.cars = [];
+  }
+
+  // Method to add a car to the dealership
+  addCar(car) {
+    this.cars.push(car);
+    console.log(`${car.brand} ${car.model} added to ${this.name}.`);
+  }
+
+  // Method to display the cars in the dealership
+  displayCars() {
+    console.log(`Cars available at ${this.name}:`);
+    this.cars.forEach((car, index) => {
+      console.log(`${index + 1}. ${car.brand} ${car.model}`);
+    });
+  }
+
+  // Method to sell a car from the dealership
+  sellCar(index) {
+    const soldCar = this.cars.splice(index, 1)[0];
+    console.log(
+      `${soldCar.brand} ${soldCar.model} sold from ${this.name}.`
+    );
+  }
+}
+
+// Create an instance of Dealership
+const dealership = new Dealership("Awesome Cars");
+
+// Add cars to the dealership
+dealership.addCar(car1);
+dealership.addCar(car2);
+
+// Display the cars available in the dealership
+dealership.displayCars();
+
+// Sell a car from the dealership
+dealership.sellCar(0);
+
+// Display the updated list of cars in the dealership
+dealership.displayCars();
+
+// Start the remaining car
+car2.start();
+
+// Stop the remaining car
+car2.stop();
+
+// ... Additional code, data structures, and algorithms can be added below
+// ...
+// ...
